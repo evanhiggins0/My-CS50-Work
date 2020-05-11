aa747a472b93754a42ccf4472deb7f7a9a857f22
evanhiggins0 <evanhiggins0@users.noreply.github.com>
2020-05-11 16:26:31 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -12,62 +12,62 @@ int main(void)
 
     if (n == 1)
     {
-        printf("# #\n");
+        printf("#  #\n");
     }
     if (n == 2)
     {
-        printf(" # #\n");
-        printf("## ##\n");
+        printf(" #  #\n");
+        printf("##  ##\n");
     }
     if (n == 3)
     {
-        printf("  # #  \n");
-        printf(" ## ##\n");
-        printf("### ###\n");
+        printf("  #  #  \n");
+        printf(" ##  ##\n");
+        printf("###  ###\n");
     }
     if (n == 4)
     {
-        printf("   # #\n");
-        printf("  ## ##\n");
-        printf(" ### ###\n");
-        printf("#### ####\n");
+        printf("   #  #\n");
+        printf("  ##  ##\n");
+        printf(" ###  ###\n");
+        printf("####  ####\n");
     }
     if (n == 5)
     {
-        printf("    # #\n");
-        printf("   ## ##\n");
-        printf("  ### ###\n");
-        printf(" #### ####\n");
-        printf("##### #####\n");
+        printf("    #  #\n");
+        printf("   ##  ##\n");
+        printf("  ###  ###\n");
+        printf(" ####  ####\n");
+        printf("#####  #####\n");
     }
     if (n == 6)
     {
-        printf("     # #\n");
-        printf("    ## ##\n");
-        printf("   ### ###\n");
-        printf("  #### ####\n");
-        printf(" ##### #####\n");
-        printf("###### ######\n");
+        printf("     #  #\n");
+        printf("    ##  ##\n");
+        printf("   ###  ###\n");
+        printf("  ####  ####\n");
+        printf(" #####  #####\n");
+        printf("######  ######\n");
     }
     if (n == 7)
     {
-        printf("      # #\n");
-        printf("     ## ##\n");
-        printf("    ### ###\n");
-        printf("   #### ####\n");
-        printf("  ##### #####\n");
-        printf(" ###### ######\n");
-        printf("####### #######\n");
+        printf("      #  #\n");
+        printf("     ##  ##\n");
+        printf("    ###  ###\n");
+        printf("   ####  ####\n");
+        printf("  #####  #####\n");
+        printf(" ######  ######\n");
+        printf("#######  #######\n");
     }
     if (n == 8)
     {
-        printf("       # #\n");
-        printf("      ## ##\n");
-        printf("     ### ###\n");
-        printf("    #### ####\n");
-        printf("   ##### #####\n");
-        printf("  ###### ######\n");
-        printf(" ####### #######\n");
-        printf("######## ########\n");
+        printf("       #  #\n");
+        printf("      ##  ##\n");
+        printf("     ###  ###\n");
+        printf("    ####  ####\n");
+        printf("   #####  #####\n");
+        printf("  ######  ######\n");
+        printf(" #######  #######\n");
+        printf("########  ########\n");
     }
 }
