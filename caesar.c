72df557c179bb44352a59ede16ef44d9a4edc702
evanhiggins0 <evanhiggins0@users.noreply.github.com>
2020-05-20 14:58:46 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -9,7 +9,7 @@ int main(int argc, string argv[])
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
-        return 0;
+        return 1;
     }
     // atoi converts ascii chars to ints
     int k = atoi(argv[1]);
